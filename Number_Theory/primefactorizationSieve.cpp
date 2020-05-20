b4820ee16ec1c232c53faa5a3b28790ec463f0d6
tusharsriMNNIT <tusharsri2803@gmail.com>
2020-05-20 11:58:33 +0530

    addedPrimeFactorThroughSieve

--- Number_Theory/primefactorizationSieve.cpp
+++ Number_Theory/primefactorizationSieve.cpp
@@ -1,9 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
+#define max 1000004
+ll leastPrime[max];
+
+void leastPrimeCalculate()
+{
+    ll i,j;
+    memset(leastPrime,-1,sizeof(leastPrime));
+    leastPrime[0]=0;
+    leastPrime[1]=1;
+    for(i=2;i*i<=max;i++)
+    {
+       if(leastPrime[i]==-1)
+       {
+           leastPrime[i]=i;
+           for(j=i*i;j<max;j+=i)
+           {
+               if(leastPrime[j]==-1)
+               {
+                   leastPrime[j]=i;
+               }
+           }
+       }
+    }
+}
 
 int main()
 {
-    
+    leastPrimeCalculate();
+    for(ll i=1;i<=10;i++)
+     cout<<leastPrime[i]<<" ";
+    cout<<endl;
     return 0;
 }
